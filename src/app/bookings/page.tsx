454677570beaca4acc0b2133c490bc6950b2d5b2
JarneDM <jarne.demeyer05@hotmail.com>
2025-11-27 09:13:52 +0100

    working on bookings page

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -2,6 +2,16 @@ export default function Bookings() {
   return (
     <div>
       <h1>Your Bookings</h1>
+      <p>Bekijk en beheer uw boekingen</p>
+
+      <div>
+        {/* Hier zou je een lijst van boekingen kunnen weergeven */}
+        <ul>
+          <li>Boeking 1</li>
+          <li>Boeking 2</li>
+          <li>Boeking 3</li>
+        </ul>
+      </div>
     </div>
   );
 }
