23468ae08add1873072aaa690a074a600e0e07de
JarneDM <jarne.demeyer05@hotmail.com>
2025-12-03 16:19:38 +0100

    styling home page

---
+++ src/components/StatsCard.tsx
@@ -0,0 +1,35 @@
+interface StatsCardProps {
+  title: string;
+  value: string;
+  subtext: string;
+  Icon: React.ElementType;
+  iconColor: string;
+  iconBg: string;
+  trendIcon?: boolean;
+}
+
+const StatsCard: React.FC<StatsCardProps> = ({
+  title,
+  value,
+  subtext,
+  Icon,
+  iconColor,
+  iconBg,
+}) => (
+  <div className="bg-white p-6 rounded-xl shadow-sm border border-gray-100 flex flex-col justify-between h-36">
+    <div className="flex justify-start items-center w-auto">
+      <h2 className="text-sm font-semibold text-gray-700 p-0 mr-2">{title}</h2>
+      <div className={`p-2 rounded-lg ${iconBg}`}>
+        <Icon className={`w-5 h-5 ${iconColor}`} />
+      </div>
+    </div>
+    <div className="mt-2">
+      <p className="text-3xl font-bold text-gray-900 flex items-center">
+        {value}
+      </p>
+      <p className="text-xs text-gray-500 mt-1">{subtext}</p>
+    </div>
+  </div>
+);
+
+export default StatsCard;
