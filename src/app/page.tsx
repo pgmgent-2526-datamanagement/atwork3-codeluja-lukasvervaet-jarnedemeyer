635a9e1686c9318cdb67883e28b306334bdf3cb2
JarneDM <jarne.demeyer05@hotmail.com>
2026-01-06 16:13:38 +0100

    add todays bookings to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,26 @@ import {
   ClockIcon,
   // ChartBarSquareIcon,
 } from "@heroicons/react/24/outline";
+import { SkeletonBookingItem } from "@/components/Loader";
 
 import StatsCard from "../components/StatsCard";
 import RefreshBookings from "@/components/AddBookingButton";
 import HomeLoader from "@/components/HomeLoader";
+
+interface Booking {
+  id: number;
+  playersCount: number;
+  startTime: string;
+  endTime: string;
+  bookingDate: string;
+  bookingDescription: string;
+  hostsRequired: number;
+  food_required: boolean;
+  is_b2b: boolean;
+  packageName: string;
+  notes: string;
+  status: string;
+}
 
 export default function Home() {
   const [bookings, setBookings] = useState<any[]>([]); // eslint-disable-line
@@ -68,11 +84,33 @@ export default function Home() {
     })();
   }, []);
 
+  const TodayBookings = (allBookings: Booking[]) => {
+    const today = new Date();
+    return allBookings.filter((booking) => {
+      const bookingDate = new Date(booking.bookingDate);
+      return (
+        bookingDate.getDate() === today.getDate() &&
+        bookingDate.getMonth() === today.getMonth() &&
+        bookingDate.getFullYear() === today.getFullYear()
+      );
+    });
+  };
+
+  const bookingsToday = TodayBookings(bookings);
+
+  const isValidDate = (dateString: string) => {
+    const date = new Date(dateString);
+    // Checks if the date object is valid AND is not the Epoch Time (plus or minus a few hours for timezone offset)
+    return (
+      date instanceof Date && !isNaN(date.getTime()) && date.getTime() > 3600000
+    ); // Check if time is greater than 1 hour after Epoch
+  };
+
   if (loading) {
     return <HomeLoader />;
   }
   return (
-    <main className="min-h-screen md:p-10 lg:p-10">
+    <main className="min-h-screen w-full max-w-[90%] md:p-10 lg:p-10">
       <div className="mb-8">
         <h1 className="text-3xl font-bold text-gray-900">Dashboard test</h1>
         <p className="text-base text-gray-500 mt-1">
@@ -82,6 +120,76 @@ export default function Home() {
 
       <div className="my-3 flex justify-end">
         <RefreshBookings onRefresh={fetchData} />
+      </div>
+
+      <div className="my-6">
+        {/* // ! when clicking open modal with all the details!! */}
+        <div className="flex overflow-x-scroll bg-white border shadow-md border-gray-100 p-4 rounded-lg mt-6 text-black flex-row-2  m-auto w-full h-auto space-x-3">
+          {bookingsToday.map((booking: Booking) => {
+            return (
+              <div key={booking.id} className="w-full">
+                <div className="flex flex-col justify-start border border-gray-100 p-4 rounded-md gap-2 shadow-sm mt-2 h-auto min-w-80 w-full">
+                  <p>
+                    <span className="font-bold">Players: </span>
+                    {booking.playersCount}
+                  </p>
+                  {/* <p>Hosts: {booking.hostsRequired}</p> */}
+
+                  <div className="flex justify-between w-[50%] text-gray-500">
+                    {/* <p>
+                        {isValidDate(booking.bookingDate) ? (
+                          new Date(booking.bookingDate).toLocaleDateString(
+                            "nl-NL",
+                            {
+                              day: "2-digit",
+                              month: "short",
+                              year: "numeric",
+                            }
+                          )
+                        ) : (
+                          <span className="text-red-500 font-semibold">
+                            Missing Date
+                          </span>
+                        )}
+                      </p> */}
+                    <p>
+                      {isValidDate(booking.startTime) &&
+                      isValidDate(booking.endTime) ? (
+                        `${new Date(booking.startTime).toLocaleTimeString(
+                          "nl-NL",
+                          { hour: "2-digit", minute: "2-digit" }
+                        )} - ${new Date(booking.endTime).toLocaleTimeString(
+                          "nl-NL",
+                          { hour: "2-digit", minute: "2-digit" }
+                        )}`
+                      ) : (
+                        <span className="text-red-500 font-semibold">
+                          Missing Time
+                        </span>
+                      )}
+                    </p>
+                  </div>
+                  <div>
+                    {/* <p>
+                        <span className="font-bold">Package: </span>
+                        {booking.packageName}
+                      </p> */}
+
+                    <p>
+                      <span className="font-semibold">Food Required:</span>
+                      {booking.food_required ? "Yes" : "No"}
+                    </p>
+
+                    <p>
+                      <span className="font-semibold">Is B2B: </span>
+                      {booking.is_b2b ? "Yes" : "No"}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
 
       <div className="grid grid-cols-1 gap-6">
