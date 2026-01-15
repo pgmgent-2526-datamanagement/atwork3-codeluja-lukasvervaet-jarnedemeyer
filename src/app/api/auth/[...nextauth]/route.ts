3d552376f0d91dfe6ed556a61eaee6902a829e3b
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2026-01-15 13:46:17 +0100

    refactor: reorder NextAuth imports for consistency

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "@/lib/client";
 import bcrypt from "bcrypt";
