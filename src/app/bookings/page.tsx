da2166473578610f355f000b83c245f32ef580c4
JarneDM <jarne.demeyer05@hotmail.com>
2026-01-16 16:59:10 +0100

    style: update layout and styling for Bookings page

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -46,17 +46,14 @@ export default function Bookings() {
   }, []);
 
   return (
-    <div className="flex flex-col justify-center items-center ml-32">
+    <div className="flex flex-col justify-center items-center mx-auto my-auto w-[80%] h-23s0 border rounded-lg shadow-lg bg-gray-200/50 pb-6">
       <header className="flex p-5 justify-between space-x-3 items-center text-center w-full h-min">
-        <h1 className="text-2xl font-semibold flex justify-center text-center underline -underline-offset-[-3px] saira-font">
-          Your Bookings
-        </h1>
-
         <select
           value={filter.type}
           onChange={(e) =>
             setFilter({ type: e.target.value as Filter["type"] })
           }
+          className="border rounded-md p-2 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           <option value="all">All Bookings</option>
           <option value="b2b">B2B Bookings</option>
@@ -69,7 +66,7 @@ export default function Bookings() {
       {loading ? (
         <SkeletonCalendarContainer />
       ) : (
-        <div className="h-160 overflow-y-scroll">
+        <div className="h-180 overflow-y-scroll">
           <BookingsCalendar bookings={filteredBookings()} />
         </div>
       )}
