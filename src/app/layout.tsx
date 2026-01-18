cd9db971cd11297b9f19a45a430b18148c627c41
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2026-01-18 21:34:42 +0100

    fix: update favicon link in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo-thepark.svg" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
